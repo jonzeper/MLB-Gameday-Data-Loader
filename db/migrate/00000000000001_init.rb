f41035207263a601bca2254bfd7c08914be58aac
Jon Zepernick <jonzeper@gmail.com>
2013-02-03 10:00:46 -0500

    Migration to create Team table

---
+++ db/migrate/00000000000001_init.rb
@@ -0,0 +1,15 @@
+class Init < ActiveRecord::Migration
+  def change
+    create_table :teams do |t|
+      t.string  :code,   limit: 3
+      t.string  :abbrev, limit: 3
+      t.integer :mlbam_id
+      t.string  :name
+      t.string  :name_full
+      t.string  :name_brief
+      t.integer :division_id
+      t.integer :league_id
+      t.string  :league, limit: 3
+    end
+  end
+end
